287bcb4596f14df4ad5f4add754bb22decc1824d
Jaden Falbee <j.falbee04@gmail.com>
2023-06-28 20:38:04 -0400

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(x) {
+    return function(fare) {
+        return fare * x;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnedDrivers) {
+    if (returnedDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } else {
+        return returnLastTwoDrivers(drivers)
+    }
+}
